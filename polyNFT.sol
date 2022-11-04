199d6b799bf8f35c72cf3e64925f4a4ebf72c84d
Anthony Proctor <anthonyproctor@Anthonys-MacBook-Pro.local>
2022-11-04 16:04:34 -0600

    added grow function

--- polyNFT.sol
+++ polyNFT.sol
@@ -9,6 +9,13 @@ contract GameItem is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    //Metadata information for each stage of the NFT on IPFS.
+    string[] IpfsUri = [
+        "https://ipfs.io/ipfs/QmYaTsyxTDnrG4toc8721w62rL4ZBKXQTGj9c9Rpdrntou/seed.json",
+        "https://ipfs.io/ipfs/QmYaTsyxTDnrG4toc8721w62rL4ZBKXQTGj9c9Rpdrntou/purple-sprout.json",
+        "https://ipfs.io/ipfs/QmYaTsyxTDnrG4toc8721w62rL4ZBKXQTGj9c9Rpdrntou/purple-blooms.json"
+    ];
+
     constructor() ERC721("GameItem", "ITM") {}
 
     function awardItem(address player, string memory tokenURI)
@@ -22,4 +29,24 @@ contract GameItem is ERC721URIStorage {
         _tokenIds.increment();
         return newItemId;
     }
+
+    function growFlower(uint256 _tokenId) public {
+        if(flowerStage(_tokenId) >= 2){return;}
+        //Get the current stage of the flower and add 1
+        uint256 newVal = flowerStage(_tokenId) + 1;
+        //store the new URI
+        string memory newUri = IpfsUri[newVal];
+        //Update the URI
+        _setTokenURI(_tokenId, newUri);
+    }
+/*
+    ********************
+    * HELPER FUNCTIONS *
+    ********************
+*/ 
+
+    //determin the stage of the flower
+    function flowerStage
+
+
 }
